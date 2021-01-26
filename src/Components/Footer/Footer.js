1c790bf7176c42b8c630185f484e13235f9bbd66
RGordon93 <contactrashawngordon@gmail.com>
2021-01-25 20:14:51 -0500

    deploy

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
 				<h6 className='footer-text'>S O F A S®️</h6>
 				<hr></hr>
 				<h6 className='footer-text'>
-					This website was developed by Rashawn Isaac Gordon using HTML, CSS & React.
+					This website was developed by Rashawn Isaac Gordon 
 				</h6>
 			</div>
 		);
